3be2297b29b736149c59312fb6c14ef30f701a3d
Alex Koval <301019a@gmail.com>
2022-11-28 21:13:00 +0200

    test-2
    fin

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+//____________________________________________________// Створюємо галерею //_______________________________//
+const galleryEl = document.querySelector('.gallery'); // шукаємо gallery
+
+const createGallery = galleryItems.reduce(
+  (acc, { preview, original, description }) => {      // перебираємо масив фото
+    let string =                                      // створюємо шаблонний рядок
+    `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" /></a>`;
+    return acc + string;                              // робимо один рядок html
+  },'');
+
+galleryEl.insertAdjacentHTML(                         // вставляємо масим фото
+  'beforeend',                                        // вибираємо місце розгортування
+  createGallery                                       // вибираємо об"єкт розгортування
+);
+
+//____________________________________________________// Клікаємо по зображенню //_______________________________//
+
+  new SimpleLightbox(".gallery a", {                  // Всі налаштування пропонує бібліотека
+    captionsData: "alt",
+    captionDelay: 300,
+    animationSpeed: 300,
+    overlay: true,
+    close: true,
+    showCounter: true,
+    fadeSpeed: 300,
+    loop: false,
+  });
 
 console.log(galleryItems);
+
